ac3136e41e0f9a6b9289b7fe8e3191a07bac60cd
AmuroRX-93 <leonyuzhewang@icloud.com>
2026-02-06 16:00:31 +0800

    update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleops/SoloBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleops/SoloBlue.java
@@ -329,9 +329,10 @@ public class SoloBlue extends CommandOpMode {
         boolean isGoalTag = (tagId == Vision.BLUE_GOAL_TAG_ID || tagId == Vision.RED_GOAL_TAG_ID);
         
         if (isGoalTag) {
-            double turretAngle = (robot.turret != null && robot.turret.isCalibrated())
-                    ? robot.turret.getAngleRadians() : 0;
-            boolean success = robot.drive.updateAbsolutePositionFromVisionWithTurret(robot.vision, turretAngle);
+            // Method expects DEGREES, not radians!
+            double turretAngleDeg = (robot.turret != null && robot.turret.isCalibrated())
+                    ? robot.turret.getAngleDegrees() : 0;
+            boolean success = robot.drive.updateAbsolutePositionFromVisionWithTurret(robot.vision, turretAngleDeg);
             if (success) {
                 hasValidPosition = true;
             }
